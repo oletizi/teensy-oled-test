5aab2c2e463f6e408a6541ad0e627fa1d97f5cdf
orion <oletizi@mac.com>
2023-11-21 12:16:40 -0800

    - added RMS indicator

--- loop.cpp
+++ loop.cpp
@@ -18,17 +18,24 @@
 #include <SerialFlash.h>
 
 // GUItool: begin automatically generated code
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+
+// GUItool: begin automatically generated code
 AudioSynthWaveform       waveform1;      //xy=119,156
 AudioFilterLadder        ladder1;        //xy=281,153
 AudioEffectEnvelope      amp_env;      //xy=439,150
 AudioOutputI2S           i2s1;           //xy=616,173
+AudioAnalyzeRMS          rms1;           //xy=616,221
 AudioConnection          patchCord1(waveform1, 0, ladder1, 0);
 AudioConnection          patchCord2(ladder1, amp_env);
 AudioConnection          patchCord3(amp_env, 0, i2s1, 0);
 AudioConnection          patchCord4(amp_env, 0, i2s1, 1);
+AudioConnection          patchCord5(amp_env, rms1);
 AudioControlSGTL5000     sgtl5000_1;     //xy=299,289
-// GUItool: end automatically generated code
-
 // GUItool: end automatically generated code
 
 Adafruit_SSD1306 display(4);
@@ -36,6 +43,7 @@ Adafruit_SSD1306 display(4);
 short counter = 0;
 short direction = -1;
 float osc_freq = 440;
+float rms_val = 0;
 __attribute__((unused)) void doSetup() {
     Serial.begin(9600);
     // Set up MIDI
@@ -56,10 +64,10 @@ __attribute__((unused)) void doSetup() {
     // This may wait forever if the SDA & SCL pins lack
     // pullup resistors
     sgtl5000_1.enable();
-    sgtl5000_1.volume(0.5); // caution: very loud - use oscilloscope only!
+    sgtl5000_1.volume(0.7); // caution: very loud - use oscilloscope only!
 
     // initialize amp envelope
-    amp_env.attack(0);
+    amp_env.attack(50);
     amp_env.hold(0);
     amp_env.decay(0);
     amp_env.sustain(1);
@@ -92,6 +100,7 @@ __attribute__((unused)) void doLoop() {
     uint16_t cpu_filt = ladder1.cpu_cycles;
     uint16_t cpu_max_filt = ladder1.cpu_cycles_max;
     uint16_t cpu_osc = waveform1.cpu_cycles;
+    rms_val = rms1.read();
     AudioInterrupts();
     //digitalWrite(LED_BUILTIN, HIGH);
     display.clearDisplay();
@@ -99,10 +108,13 @@ __attribute__((unused)) void doLoop() {
     display.setCursor(0, 8);
     //display.printf("Count: %d\n", counter);
     display.printf("Ofreq: %f\n", osc_freq);
+    display.printf("RMS  : %f\n", rms_val);
     display.printf("F cpu: %d, m: %d\n", cpu_filt, cpu_max_filt);
-    display.printf("O cpu : %d\n", cpu_osc);
     //display.drawCircle(counter, 16, 5, WHITE);
-    display.drawFastVLine(counter - 1, 0, 7, WHITE);
+    float rms_indicator_value = scale(rms_val, 0, 1, 0, 127, 1);
+    //display.drawFastVLine(rms_indicator_value, 0, 7, WHITE);
+    display.fillRect(0, 0, rms_indicator_value, 7, WHITE);
+
     display.display();
 
     digitalWrite(LED_BUILTIN, LOW);
@@ -137,4 +149,19 @@ void handleControlChange(byte channel, byte control, byte value) {
 float m2f(int note) {
     float a = 440; //frequency of A (coomon value is 440Hz)
     return (a / 32) * pow(2, ((note - 9) / 12.0));
+}
+
+float scale(float in, float inlow, float inhigh, float outlow, float outhigh, float power) {
+    float value;
+    float inscale = safediv(1., inhigh - inlow);
+    float outdiff = outhigh - outlow;
+
+    value = (in - inlow) * inscale;
+    if (value > 0.0)
+        value = pow(value, power);
+    else if (value < 0.0)
+        value = -pow(-value, power);
+    value = (value * outdiff) + outlow;
+
+    return value;
 }
--- loop.h
+++ loop.h
@@ -14,6 +14,10 @@ __attribute__((unused)) void doLoop();
 void handleNoteOn(byte channel, byte note, byte velocity);
 void handleNoteOff(byte channel, byte note, byte velocity);
 void handleControlChange(byte channel, byte control, byte value);
+float scale(float in, float inlow, float inhigh, float outlow, float outhigh, float power);
+inline float safediv(float num, float denom) {
+    return denom == 0. ? (float) 0. : (float) (num / denom);
+}
 float m2f(int note);
 
 #endif //ARDUINO_CLI_TEST_FOO_H
